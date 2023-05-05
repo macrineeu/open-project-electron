40b059d31bec57eb00778204d3c7e65378623b1e
Matheus Macrineu <macrineu.developer@gmail.com>
2023-05-04 22:37:45 -0300

    Adiciona novo projeto a lista de menu

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const { resolve, basename } = require('path');
-const { app, Menu, Tray, dialog } = require('electron');
+const { app, Menu, Tray, dialog, MenuItem } = require('electron');
 const Store = require('electron-store');
+const { spawn } = require('cross-spawn');
 
 const schema = {
     projects: {
         type: 'string'
     }
 }
+
 
 const store = new Store({ schema });
 
@@ -15,27 +17,37 @@ app.on('ready', () => {
     const storedProjects = store.get('projects');
     const projects = storedProjects ? JSON.parse(storedProjects) : [];
 
-    console.log(projects);
+    const items = projects.map((project) => {
+        return { label: project.name, click: () => spawn.sync('code', [project.path]) }
+    });
 
     const contextMenu = Menu.buildFromTemplate([
+        ...items,
         {
-            label: 'Item1', type: 'radio', checked: true, click: () => {
-                //Seleciona pasta de arquivos
-                const path = dialog.showOpenDialog({
-                    properties: ['openDirectory']
-                }).then(result => {
-                    let path = result.filePaths;
-
-                    store.set('projects', JSON.stringify([...projects, {
-                        path,
-                        name: basename(result.filePaths[0])
-                    }]))
-                }).catch(err => {
-                    console.log(err)
-                })
-            }
+            type: 'separator',
         }
     ]);
+
+    contextMenu.insert(0, new MenuItem({
+        label: 'Adicionar Projeto ...', click: () => {
+            //Seleciona pasta de arquivos
+            const path = dialog.showOpenDialog({
+                properties: ['openDirectory']
+            }).then(result => {
+                let path = result.filePaths;
+                let name = basename(result.filePaths[0]);
+
+                store.set('projects', JSON.stringify([...projects, {
+                    path,
+                    name
+                }]));
+
+                const item = new MenuItem({ label: name, click: () => spawn.sync('code', [path]) })
+
+                contextMenu.append(item);
+            });
+        }
+    }));
 
     tray.setToolTip('This is my first application');
     tray.setContextMenu(contextMenu);
