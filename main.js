4e5329cb3b64a7f8f58bac18308442d7881e08c5
Matheus Macrineu <macrineu.developer@gmail.com>
2023-05-04 21:59:13 -0300

    add console.log file path selected

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 const { resolve } = require('path');
-const { app, Menu, Tray } = require('electron');
+const { app, Menu, Tray, dialog } = require('electron');
+const Store = require('electron-store');
+
+const schema = {
+    projects: {
+        type: 'array'
+    }
+}
+
+const store = new Store({ schema });
 
 app.on('ready', () => {
     const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
 
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'Item1', type: 'radio', checked: true }
+        {
+            label: 'Item1', type: 'radio', checked: true, click: () => {
+                //Seleciona pasta de arquivos
+                const path = dialog.showOpenDialog({
+                    properties: ['openDirectory']
+                }).then(result => {
+                    console.log(result.filePaths)
+                }).catch(err => {
+                    console.log(err)
+                })
+            }
+        }
     ]);
 
     tray.setToolTip('This is my first application');
